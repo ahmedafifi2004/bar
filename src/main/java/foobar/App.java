0eb827445d03cc9b0b74211fb394134b7c3b2663
fahad21080 <fahadit21@gmail.com>
2019-08-25 12:32:34 +0300

    test commit

--- src/main/java/foobar/App.java
+++ src/main/java/foobar/App.java
@@ -11,6 +11,9 @@ public class App
     public static void main( String[] args )
     {
 
+
+        System.out.println("Test Check in");
+
         Flyway flyway = Flyway.configure().dataSource("jdbc:h2:file:./target/foobar", "sa", null).load();
 
         // Start the migration
