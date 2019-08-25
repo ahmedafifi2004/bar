73b6b229cc5dd2361152a97d876ff2ef629ada4d
fahad21080 <fahadit21@gmail.com>
2019-08-25 12:28:22 +0300

    Initial commit

---
+++ src/main/java/foobar/App.java
@@ -0,0 +1,23 @@
+package foobar;
+
+/**
+ * Hello world!
+ *
+ */
+
+import org.flywaydb.core.Flyway;
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:file:./target/foobar", "sa", null).load();
+
+        // Start the migration
+        flyway.migrate();
+
+
+
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/resources/db/migration/V1__Create_person_table.sql
@@ -0,0 +1,13 @@
+create table PERSON (
+    ID int not null,
+    NAME varchar(100) not null
+);
+
+create table ADDRESS (
+
+ID int not null ,
+FLAT_NO varchar(100) not null,
+STREET_NO varchar(100) not null,
+ZONE_NO varchar(100) not null,
+PRIMARY KEY(ID)
+);
